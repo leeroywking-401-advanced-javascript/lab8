16cbd3a971f72dd715ec4cc3ccf9dc81dcf7ca9a
leeroywking <leeroywking@gmail.com>
2019-07-17 21:03:24 -0700

    finally things are wired

---
+++ src/models/mongo.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/** Class representing a generic mongo model. */
+class Model {
+  /**
+   * Model Constructor
+   * @param schema {object} - mongo schema
+   */
+  constructor(schema) {
+    this.schema = schema
+  }
+
+  /**
+   * Retrieves one or more records
+   * @param _id {string} optional mongo record id
+   * @returns {count:#,results:[{*}]} | {*}
+   */
+  get(_id) {
+    if (_id) {
+      return this.schema.find({ _id });
+    }
+    else {
+      return this.schema.find({});
+    }
+  }
+
+  /**
+   * Create a new record
+   * @param record {object} matches the format of the this.schema
+   * @returns {*}
+   */
+  create(record) {
+    let newRecord = new this.schema(record);
+    return newRecord.save();
+  }
+
+  /**
+   * Replaces a record in the database
+   * @param _id {string} Mongo Record ID
+   * @param record {object} The record data to replace. ID is a required field
+   * @returns {*}
+   */
+  update(_id, record) {
+    return this.schema.findByIdAndUpdate(_id, record, { new: true });
+  }
+
+  /**
+   * Deletes a recod in the model
+   * @param _id {string} Mongo Record ID
+   * @returns {*}
+   */
+  delete(_id) {
+    return this.schema.findByIdAndDelete(_id);
+  }
+
+}
+
+module.exports = Model;
